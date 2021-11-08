169959e621983d047b520e1c2fccbc28796c160c
Valentine Panchin <vpanchin@gmail.com>
2021-11-08 12:58:58 +0200

    feat: flipping squares (#30)
    Co-authored-by: Valentine Panchin <vpanchyn@markerr.com>

--- js/packages/web/src/views/pack/components/Card/index.tsx
+++ js/packages/web/src/views/pack/components/Card/index.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Card = ({ value, onClick }: { value: number; onClick: () => void }) => (
   <div key={value} className="pack-view__block" onClick={onClick}>
     <div className="pack-view__square">
-      <span>{value + 1}</span>
+      <div className="pack-view__square__front">
+        <span>{value + 1}</span>
+      </div>
+      <div className="pack-view__square__back">
+        <span>Open</span>
+      </div>
     </div>
   </div>
 );
